97b5647d9678a99b327375e697abc8912e12b15a
AHN <80368668+ands64013@users.noreply.github.com>
2022-03-06 22:48:07 +0900

    added server

--- 03.NODEJS-SERVER/app.js
+++ 03.NODEJS-SERVER/app.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const fs = require("fs");
+const { buffer } = require("stream/consumers");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
@@ -9,20 +10,31 @@ const server = http.createServer((req, res) => {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
     res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message123"><button type="submit">Send</button></form></body>'
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
     res.write("</html>");
     return res.end();
   }
 
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY");
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+      console.log(parsedBody);
+    });
+
     res.statusCode = 302;
     res.setHeader("Location", "/");
     return res.end();
   }
 
-  console.log(req.url, req.method, req.headers);
+  //console.log(req.url, req.method, req.headers);
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
